383b6ebd82dd2452d66ac0ab9b4f308d1d9d0c60
MOshima-PIFSC <megumi.oshima@noaa.gov>
2022-02-08 16:08:06 -0500

    added mcmc run

--- r_scripts/create_boot_dat.R
+++ r_scripts/create_boot_dat.R
@@ -1,11 +1,12 @@
 ### Creating dummy data used for running bootstrap
+## Run this after creating starting states - starting states are in the start_pop_i folder so run bootstrap in there for creating first 5 years of data then copy files to OM and EM folders and continue simulations from there
 
 library(r4ss)
 library(tidyverse)
 
 ## args is a vector that will be specified on work script but for now is a vector that contains: OM_scenario name, iteration (1-100)
 
-args <- c("perfect", 1)
+args <- c("perfect", 2)
 om.scenario <- args[1]
 iteration <- args[2]
 
@@ -13,14 +14,7 @@ dir. <- paste0(getwd(), "/vermilion_snapper_manuscript/with_comp")
 dir.boot <- file.path(dir., paste0("start_pop_", iteration))
 #to get functions like splt.recombine
 source(file.path(getwd(), "vermilion_snapper_manuscript", "r_scripts", "utils.R"))
-## Copy files into boot directory
 
-## Depending on order of scripts, this might already be done ###############################
-files. <- list("vs_envM2.dat", "control.ss_new", "forecast.ss", "starter.ss", "ss.exe")
-files.path <- file.path(dir., files.)
-file.copy(files.path, file.path(dir.boot), overwrite = TRUE)
-file.rename(file.path(dir.boot, "control.ss_new"), file.path(dir.boot, "vs.ctl"))
-############################################################################################
 
 proj.index.full <-
   read.csv(file.path(dir., "../TidyData/RS.biomass.report.csv"))
@@ -48,11 +42,11 @@ n.years.fwd <- 5
 dat. <- SS_readdat_3.30(file = file.path(dir.boot, "vs_envM2.dat"))
 
 #year starting data generation from 
-start.yr <- dat.$endyr + 1
+#start.yr <- dat.$endyr + 1
 
 ## Updating DAT file
-old.endyr <- dat.$endyr
-dat.$endyr <- dat.$endyr + n.years.fwd 
+# old.endyr <- dat.$endyr
+# dat.$endyr <- dat.$endyr + n.years.fwd 
 
 ## Find patterns in historical data
 cpue.patterns <- dat.$CPUE %>% 
@@ -91,7 +85,7 @@ catch <- data.frame(
   year = rep(seq(start.yr, start.yr + n.years.fwd - 1), nrow(catch.patterns)),
   seas = rep(1, n.years.fwd*nrow(catch.patterns)),
   fleet = rep(unique(catch.patterns$fleet), each = n.years.fwd),
-  catch = rep(0, n.years.fwd*nrow(catch.patterns)),
+  catch = rep(0.001, n.years.fwd*nrow(catch.patterns)),
   catch_se = rep(catch.patterns$mean.se, each = n.years.fwd)
 )
 
@@ -208,7 +202,7 @@ SS_writedat_3.30(dat., outfile = file.path(dir.boot, "vs_envM2.dat"), overwrite 
 ## Update Starter for bootstrap files
 starter <- SS_readstarter(file = file.path(dir.boot, "starter.ss"))
 starter$N_bootstraps <- 3
-starter$init_values_src <- 0
+starter$init_values_src <- 1
 starter$last_estimation_phase <- 0
 SS_writestarter(starter, dir = dir.boot, overwrite = TRUE)
 
